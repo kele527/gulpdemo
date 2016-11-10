10ebecf7b245b543f29d526f4e588a93adc4990b
kele527 <ycommon@qq.com>
2016-11-10 12:10:28 +0800

    readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # gulp demo
 《前端构建工具》课程demo
 ##实现功能
-css,js 压缩
-css,js 合并
-资源嵌入（html, js，css，图片内联）
-本地调试服务器，自动刷新
+    css,js 压缩
+    css,js 合并
+    资源嵌入（html, js，css，图片内联）
+    本地调试服务器，自动刷新
 
 ##usage
-先执行 npm i  安装项目依赖， 然后允许 gulp 命令
+先执行 `npm i`  安装项目依赖， 然后运行 `gulp` 命令
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 /**
- * Created by yangjason on 16/11/7.
+ * gulp demo
+ *
+ * by kele527
  */
 
 var del=require('del');
 var gulp=require('gulp');
 var uglify=require('gulp-uglify');
-var mincss=require('gulp-clean-css');
-var inline=require('gulp-inline-source');
-var include=require('gulp-include');
+var mincss=require('gulp-clean-css');//压缩css
+var inline=require('gulp-inline-source'); //资源内联 （主要是js，css，图片）
+var include=require('gulp-include'); //资源内联（主要是html片段）
 var sequence=require('gulp-sequence');
-var useref=require('gulp-useref');
+var useref=require('gulp-useref'); //合并文件
 var gulpif=require('gulp-if');
-var print=require('gulp-print');
-var connect=require('gulp-connect');
+var print=require('gulp-print'); //打印命中的文件
+var connect=require('gulp-connect'); //本地服务器
 
 
 //清理构建目录
@@ -39,13 +41,12 @@ gulp.task('minjs',function () {
 
 gulp.task('html', function () {
     return gulp.src('./src/*.html')
-        .pipe(inline())
-        .pipe(include())
-        .pipe(print())
-        .pipe(useref())
+        .pipe(inline())//把js内联到html中
+        .pipe(include())//把html片段内联到html主文件中
+        .pipe(useref())//根据标记的块  合并js或者css
         .pipe(gulpif('*.js',uglify()))
         .pipe(gulpif('*.css',mincss()))
-        .pipe(connect.reload())
+        .pipe(connect.reload()) //重新构建后自动刷新页面
         .pipe(gulp.dest('dist'));
 });
 
@@ -59,7 +60,7 @@ gulp.task('connect', function() {
     });
 });
 
-
+//sequence的返回函数只能运行一次 所以这里用function cb方式使用
 gulp.task('watchlist',function (cb) {
     sequence('clean',['mincss','minjs','html'])(cb)
 });
@@ -69,6 +70,7 @@ gulp.task('watch',function () {
 });
 
 
+//中括号外面的是串行执行， 中括号里面的任务是并行执行。
 gulp.task('default',function (cb) {
     sequence('clean',['mincss','minjs','html','connect'],'watch')(cb)
 });
